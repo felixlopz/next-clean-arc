d94a74e6d3783470061ecb52bf907454d9aa7f50
Felix Lopez <felixlopzd@gmail.com>
2024-12-17 21:55:25 -0400

    test commit

--- app/(routes)/sign-in/page.tsx
+++ app/(routes)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from '@/app/features/auth/components/sign-in-form';
 
 export default function SignInPage() {
   return (
-    <div className="max-w-screen-sm mx-auto">
+    <div className="mx-auto max-w-screen-sm">
       <SignInForm />
     </div>
   );
--- app/(routes)/sign-up/page.tsx
+++ app/(routes)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from '@/app/features/auth/components/sign-up-form';
 
 export default function SignUpPage() {
   return (
-    <div className="max-w-screen-sm mx-auto">
+    <div className="mx-auto max-w-screen-sm">
       <SignUpForm />
     </div>
   );
